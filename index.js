1292563f055e8fc1f6fbeebfe6d7059af7c30a3c
shisama <shisama07@gmail.com>
2022-01-15 00:05:22 +0900

    chore: add eslint for development

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 module.exports = {
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-config-prettier'
-  ],
+  extends: ["stylelint-config-standard", "stylelint-config-prettier"],
   rules: {
     // We don't care about the following coding styles.
-    'comment-empty-line-before': null,
-    'custom-property-empty-line-before': null,
-    'rule-empty-line-before': null,
-  }
+    "comment-empty-line-before": null,
+    "custom-property-empty-line-before": null,
+    "rule-empty-line-before": null,
+  },
 };
