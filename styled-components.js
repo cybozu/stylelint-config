9e6f556bb846c5761ad33c8e6f496c15af572d25
Masashi Hirano <shisama07@gmail.com>
2022-01-17 23:34:42 +0900

    chore: add eslint for development (#4)
    * chore: add eslint for development
    
    * chore: add lint for ci

--- styled-components.js
+++ styled-components.js
@@ -1,9 +1,6 @@
 module.exports = {
-  extends: [
-    './index.js',
-    'stylelint-config-styled-components'
-  ],
-  customSyntax: '@stylelint/postcss-css-in-js',
+  extends: ["./index.js", "stylelint-config-styled-components"],
+  customSyntax: "@stylelint/postcss-css-in-js",
   rules: {
     // These rules have problems with styled-components
     // This rule cannot recognize template literals with line feeds properly.
@@ -13,13 +10,13 @@ module.exports = {
     // )});
     // OK
     // background-image: url(${toStaticUrl('/img/foo.png')});
-    'function-url-quotes': null,
+    "function-url-quotes": null,
     // This rule cannot recognize CSS functions in template literals
     // Error
     // transform: ${({ rotate }) => `rotate(${rotate}deg)`};
     // OK
     // transform: ${({ rotate }) => `rotate(${rotate}deg) `};
-    'function-whitespace-after': null,
-    'value-keyword-case': null, // https://github.com/stylelint/stylelint/issues/4953
-  }
+    "function-whitespace-after": null,
+    "value-keyword-case": null, // https://github.com/stylelint/stylelint/issues/4953
+  },
 };
--- test/index.test.js
+++ test/index.test.js
@@ -6,31 +6,34 @@ const { it, expect } = require("@jest/globals");
 const styledComponentsConfig = require("../styled-components");
 const config = require("../index");
 
-const getSnapshot = result => result.results.map(({ warnings}) => warnings.map(({ text, ...rest}) => rest));
-const getCode = file => fs.readFileSync(path.resolve(__dirname, 'fixtures', file), 'utf-8');
+const getSnapshot = (result) =>
+  result.results.map(({ warnings }) =>
+    warnings.map(({ text, ...rest }) => rest)
+  );
+const getCode = (file) =>
+  fs.readFileSync(path.resolve(__dirname, "fixtures", file), "utf-8");
 
-it('should not raise any errors with valid.css',　async () => {
+it("should not raise any errors with valid.css", async () => {
   const result = await stylelint.lint({
-    code: getCode('valid.css'),
+    code: getCode("valid.css"),
     config,
   });
   expect(result.errored).toBeFalsy();
 });
 
-it('should raise errors with invalid.css',　async () => {
+it("should raise errors with invalid.css", async () => {
   const result = await stylelint.lint({
-    code: getCode('invalid.css'),
+    code: getCode("invalid.css"),
     config,
   });
   expect(result.errored).toBeTruthy();
   expect(getSnapshot(result)).toMatchSnapshot();
-
 });
 
-it('should verify CSS in styled-components',　async () => {
+it("should verify CSS in styled-components", async () => {
   const result = await stylelint.lint({
-    code: getCode('Styled.tsx'),
-    config: styledComponentsConfig
+    code: getCode("Styled.tsx"),
+    config: styledComponentsConfig,
   });
   expect(result.errored).toBeTruthy();
   expect(getSnapshot(result)).toMatchSnapshot();
