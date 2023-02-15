4e6bcff350e6495e067d5f63b521e4ec2f8842ba
Ogura Katsuya <ogura@cybozu.co.jp>
2023-02-15 17:20:55 +0900

    chore: Remove Deprecated Rule

--- styled-components.js
+++ styled-components.js
@@ -11,12 +11,6 @@ module.exports = {
     // OK
     // background-image: url(${toStaticUrl('/img/foo.png')});
     "function-url-quotes": null,
-    // This rule cannot recognize CSS functions in template literals
-    // Error
-    // transform: ${({ rotate }) => `rotate(${rotate}deg)`};
-    // OK
-    // transform: ${({ rotate }) => `rotate(${rotate}deg) `};
-    "function-whitespace-after": null,
     "value-keyword-case": null, // https://github.com/stylelint/stylelint/issues/4953
   },
 };
